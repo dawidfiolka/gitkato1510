10149ced15f24aa4bfce90d536610ea60d3f2476
Dawid Fiolka <115869314+dawidfiolka@users.noreply.github.com>
2022-10-28 22:32:42 +0200

    Add files via upload

---
+++ funkcje.js
@@ -0,0 +1,6 @@
+function nazwa_funkcji(wartość_funkcji) {
+    console.log("Podatek VAT od ceny netto "+wartość_funkcji+ " zł wynosi "+ wartość_funkcji *0.23+" zł.");
+    console.log("Brutto = " +wartość_funkcji *1.23 + "zł") + wartość_funkcji
+}
+nazwa_funkcji(200);
+nazwa_funkcji(100);
